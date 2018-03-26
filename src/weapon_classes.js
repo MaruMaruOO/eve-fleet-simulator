67953f0e350a6dfbb0f8efa7855416e4fc755c43
Maru Maru <donotreplytosomeguy@gmail.com>
2018-03-26 02:24:06 -0400

    Added crude targeted doomsday implementation

--- src/weapon_classes.js
+++ src/weapon_classes.js
@@ -12,41 +12,60 @@ class PendingAttack {
 
 class MissileStats {
   sigRadius: number;
+  baseSigRadius: number;
   expVelocity: number;
+  baseExpVelocity: number;
   travelVelocity: number;
+  baseTravelVelocity: number;
   damageReductionFactor: number;
   tracking: number = 0;
+  baseTracking: number = 0;
   falloff: number = 0;
+  baseFalloff: number = 0;
   constructor(wep: WeaponData) {
     this.sigRadius = wep.explosionRadius;
+    this.baseSigRadius = wep.explosionRadius;
     this.expVelocity = wep.explosionVelocity;
+    this.baseExpVelocity = wep.explosionVelocity;
     this.damageReductionFactor = wep.damageReductionFactor;
     this.travelVelocity = wep.maxVelocity;
+    this.baseTravelVelocity = wep.maxVelocity;
   }
 }
 
 class TurretStats {
   sigRadius: number = 0;
+  baseSigRadius: number = 0;
   expVelocity: number = 0;
+  baseExpVelocity: number = 0;
   travelVelocity: number = 0;
+  baseTravelVelocity: number = 0;
   damageReductionFactor: number = 0;
   tracking: number;
+  baseTracking: number;
   falloff: number;
+  baseFalloff: number;
   constructor(wep: WeaponData) {
     this.tracking = wep.tracking;
+    this.baseTracking = wep.tracking;
     this.falloff = wep.falloff;
+    this.baseFalloff = wep.falloff;
   }
 }
 
 class SmartBombStats {
   sigRadius: number = 0;
+  baseSigRadius: number = 0;
   expVelocity: number = 0;
+  baseExpVelocity: number = 0;
   travelVelocity: number = 0;
+  baseTravelVelocity: number = 0;
   damageReductionFactor: number = 0;
-  tracking: number;
+  tracking: number = 10000;
+  baseTracking: number = 10000;
   falloff: number;
+  baseFalloff: number = 0;
   constructor() {
-    this.tracking = 10000;
     this.falloff = 0;
   }
 }
@@ -60,6 +79,7 @@ class Weapon {
   stats: TurretStats | MissileStats | SmartBombStats;
   pendingDamage: PendingAttack[] = [];
   optimal: number;
+  baseOptimal: number;
   dps: number;
   autonomousMovement: boolean;
   getDamageDelay(distance: number): number {
@@ -73,6 +93,7 @@ class Weapon {
     this.damage = wep.volley;
     this.reload = 1000 * (wep.volley / wep.dps);
     this.optimal = wep.optimal;
+    this.baseOptimal = wep.optimal;
     this.dps = wep.dps;
     this.autonomousMovement = false;
     if (this.type === 'Turret') {
@@ -85,8 +106,19 @@ class Weapon {
       this.autonomousMovement = true;
       this.stats.travelVelocity = wep.maxSpeed;
     } else if (this.type === 'SmartBomb') {
-      this.type = 'Turret';
-      this.stats = new SmartBombStats();
+      if (wep.name.includes('Doomsday')) {
+        this.type = 'Missile';
+        wep.explosionRadius = 5000;
+        wep.explosionVelocity = 2000;
+        wep.damageReductionFactor = 0.01;
+        wep.maxVelocity = 12000;
+        this.stats = new MissileStats(wep);
+        this.optimal = 300000;
+        this.baseOptimal = 300000;
+      } else {
+        this.type = 'Turret';
+        this.stats = new SmartBombStats();
+      }
     } else if (this.type === 'Fighter') {
       wep.maxVelocity = 7000;
       this.type = 'Missile';
