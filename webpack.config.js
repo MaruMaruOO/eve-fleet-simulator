ab2ad65a0ff8a1500c4ee3827bec61ccd8ad3bab
Maru Maru <donotreplytosomeguy@gmail.com>
2018-09-18 01:21:59 -0400

    Have webpack inline larger fonts used for symbols.

--- webpack.config.js
+++ webpack.config.js
@@ -41,7 +41,7 @@ module.exports = [{
                      },
                      {
                        test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-                       loader: 'url-loader?limit=10000&mimetype=application/font-woff'
+                       loader: 'url-loader?limit=1000000&mimetype=application/font-woff'
                      },
                      {
                        test: /\.(ttf|eot)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
