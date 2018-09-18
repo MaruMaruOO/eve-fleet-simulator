f54e7547fefff4adf5434e61738ddce7160469dc
Maru Maru <donotreplytosomeguy@gmail.com>
2018-09-18 03:51:28 -0400

    Fixed outdated npm scripts

--- packaging_scripts/make_ship_render_icons.sh
+++ packaging_scripts/make_ship_render_icons.sh
@@ -1,3 +1,3 @@
-cd /home/stock/scripts/semantic_eve_sim/src/eve_icons/renders/Renders
+cd ../src/eve_icons/renders/Renders
 find -maxdepth 1 -iname "*.png" -exec convert -scale 35 {} w35/{} \;
 find -maxdepth 1 -iname "*.png" -exec convert -scale 80 {} w80/{} \;
