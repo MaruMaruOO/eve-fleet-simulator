791b8bf597e7c93420766a9fe4e9b99933f10d95
Maru Maru <donotreplytosomeguy@gmail.com>
2018-09-27 08:43:52 -0400

    Add correct ccp licencing

--- src/react_components/about_page.jsx
+++ src/react_components/about_page.jsx
@@ -433,14 +433,21 @@ function AboutPage() {
     <div className="pageMainContentWrapper">
       <Container className="pageContainer">
         <Header as="h5" attached="top">
-          CCP (REPLACE WITH ACTUAL LICENCE)
+          CCP Copyright Notice
         </Header>
         <Segment attached>
           EVE Online and the EVE logo are the registered trademarks of CCP hf.
-          All rights are reserved worldwide.
-          All other trademarks are the property of their respective owners.
-          EVE Online, the EVE logo, EVE and all associated logos and designs
-          are the intellectual property of CCP hf.
+          All rights are reserved worldwide. All other trademarks are the property of their
+          respective owners. EVE Online, the EVE logo, EVE and all associated logos and
+          designs are the intellectual property of CCP hf. All artwork, screenshots,
+          characters, vehicles, storylines, world facts or other recognizable features
+          of the intellectual property relating to these trademarks are likewise the
+          intellectual property of CCP hf. CCP hf. has granted permission to Eve Fleet Simulator
+          to use EVE Online and all associated logos and designs for promotional and
+          information purposes on its website but does not endorse, and is not in any
+          way affiliated with, Eve Fleet Simulator.
+          CCP is in no way responsible for the content on or functioning of this
+          website, nor can it be liable for any damage arising from the use of this website.
         </Segment>
         <Header as="h5" attached="top">
           FAQ
