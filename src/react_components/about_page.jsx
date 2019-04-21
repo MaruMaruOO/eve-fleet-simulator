6d31e351c6083287a72ecdd2a9dda94e779bde2e
Maru Maru <donotreplytosomeguy@gmail.com>
2019-04-21 04:04:36 -0400

    Updated FAQ with new questions.

--- src/react_components/about_page.jsx
+++ src/react_components/about_page.jsx
@@ -66,7 +66,100 @@ const faqAnswer6 = (
     <a href="https://github.com/MaruMaruOO/eve-fleet-simulator/blob/master/resources/Limitations.md"> List of known limitations</a>.<br />
     If your question is still unanswered consider
     <a href="https://github.com/MaruMaruOO/eve-fleet-simulator/issues"> opening a github issue </a>
-    if it&apos;s a bug or contacting the developer so it can be added to the FAQ or documentation.`
+    if it&apos;s a bug or contacting the developer so it can be added to the FAQ or documentation.
+  </div>
+);
+
+const faqAnswer7 = (
+  <div>
+    Imported fits are saved just for you. Specifically they are stored in your browsers local storage.
+  </div>
+);
+
+const faqAnswer8 = (
+  <div>
+    The AI doesn't swap off targets at this time. In order to change target priories you can either drag table rows or type it in.
+  </div>
+);
+
+const faqAnswer9 = (
+  <div>
+    No, to avoid giving one side an advantage EFS sequences actions so the side doesn&apos;t impact the outcome.<br />
+    For example a tick will apply both sides weapons before either side removes loses.<br />
+    Similarly ships will move based off where the opposing ships were in the previous tick.<br />
+    This doesn't cause meaningful inaccuracies because EFS uses 50ms ticks that are shorter than most players pings to the server.
+  </div>
+);
+
+const faqAnswer10 = (
+  <div>
+    Yes, you can see most upcoming plans by looking at the
+    <a href="https://github.com/MaruMaruOO/eve-fleet-simulator/issues"> GitHub issues page</a>.<br />
+    You can also leave comments or post suggestions there.
+  </div>
+);
+
+const faqAnswer11 = (
+  <div>
+    Take a look at the
+    <a href="https://github.com/MaruMaruOO/eve-fleet-simulator/blob/master/resources/Limitations.md"> list of known limitations</a>.<br />
+    If your query isn't impacted by anything listed then the implementation should match the game.<br />
+    If it doesn't seem to match the list or game please open an
+    <a href="https://github.com/MaruMaruOO/eve-fleet-simulator/issues"> issue on GitHub </a>so it can be fixed.
+  </div>
+);
+
+const faqAnswer12 = (
+  <div>
+    Yes, in order to accommodate the Eve player base both these numbers are actually text boxes.<br />
+    So press/click the number and type in that nice round figure.
+  </div>
+);
+
+const faqAnswer13 = (
+  <div>
+    EFS uses it's own data format for imports, you can't export fits from zkill or the like.<br />
+    You must export fits from pyfa using the EFS format (Edit -> To Clipboard -> EFS -> OK).<br />
+    If you're doing this, pyfa is up to date and you still get the error, please report it as a bug.
+  </div>
+);
+
+const faqAnswer14 = (
+  <div>
+    Each fit will position independently, selecting it's own anchor and following it around.<br />
+    Ships will try to follow their anchor but opposing tackle can cause them to get separated and fall behind.<br />
+    For specifics see the answer to "How does the AI determine at what range it wants to be at?".
+  </div>
+);
+
+const faqAnswer15 = (
+  <div>
+    Ships with remote repair start locking as soon as a friendly takes damage.<br />
+    After that they cycle the repairs, repairing at the start of the cycle for shields or the end for armor/hull.
+    Target resistance is factored into the repair.<br />
+    Repair scaling uses an approximate formula that should always be within 5%.
+    In practice it's typically much closer, especially at large scales.<br />
+    There's also a system in place to avoid massively over repairing and having the target die between cycles.
+  </div>
+);
+
+const faqAnswer16 = (
+  <div>
+    Each fit tests to find it's ideal range every 10 simulated seconds.<br />
+    Logi tries to take as little damage as possible while staying in repair range.<br />
+    Support ships try and take as little damage as possible while also applying their ewar.
+    If required support ships will also unanchor in order to keep applying ewar to their target/s.
+    It's particularly common to see this when they are trying to spread scrams across the opposing fleet.<br />
+    Damage dealers and other ships try to keep <code>damage done / damage taken</code> as high as possible.
+    They will also prioritize keeping application over 30% if possible, so they won't shoot from deep falloff just because the ratio is higher.
+  </div>
+);
+
+const faqAnswer17 = (
+  <div>
+    Make sure your phone is in landscape mode when browsing the EFS website on mobile.<br />
+    A portrait mode is planned but for now landscape should hopefully work fairly well.<br />
+    Mobile apps are possible if the demand is there but they are basically just a wrapper around a local copy of the website.
   </div>
 );
 
@@ -100,6 +193,61 @@ const faq = [
     key: 'faq5',
     title: 'How do I get started?',
     content: { content: faqAnswer5, key: 'faqAnswer5' },
+  },
+  {
+    key: 'faq7',
+    title: 'Are imported fits on the website saved for everyone or only for me?',
+    content: { content: faqAnswer7, key: 'faqAnswer7' },
+  },
+  {
+    key: 'faq8',
+    title: 'I got into some weird deadlock situations where both fleet\'s targets caught reps and they refused to switch.',
+    content: { content: faqAnswer8, key: 'faqAnswer8' },
+  },
+  {
+    key: 'faq9',
+    title: 'Why do identical fleets perfectly wipe each other out, shouldn\'t the side that goes first win?',
+    content: { content: faqAnswer9, key: 'faqAnswer9' },
+  },
+  {
+    key: 'faq12',
+    title: 'Can I set a specific number for range or replay speed?',
+    content: { content: faqAnswer12, key: 'faqAnswer12' },
+  },
+  {
+    key: 'faq13',
+    title: 'When I tried to import a fit I got "Unable to parse fit information".',
+    content: { content: faqAnswer13, key: 'faqAnswer13' },
+  },
+  {
+    key: 'faq14',
+    title: 'How do anchors and positioning work?',
+    content: { content: faqAnswer14, key: 'faqAnswer14' },
+  },
+  {
+    key: 'faq15',
+    title: 'How do remote repairs work?',
+    content: { content: faqAnswer15, key: 'faqAnswer15' },
+  },
+  {
+    key: 'faq16',
+    title: 'How does the AI determine at what range it wants to be at?',
+    content: { content: faqAnswer16, key: 'faqAnswer16' },
+  },
+  {
+    key: 'faq11',
+    title: 'Does EFS simulate or handle X?',
+    content: { content: faqAnswer11, key: 'faqAnswer11' },
+  },
+  {
+    key: 'faq17',
+    title: 'Are you planning a mobile version for this?',
+    content: { content: faqAnswer17, key: 'faqAnswer17' },
+  },
+  {
+    key: 'faq10',
+    title: 'Any future plans on expanding this?',
+    content: { content: faqAnswer10, key: 'faqAnswer10' },
   },
   {
     key: 'faq6',
