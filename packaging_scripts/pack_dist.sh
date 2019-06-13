8eec78147488dce7ab955251843d3615f515c885
Maru Maru <donotreplytosomeguy@gmail.com>
2019-06-13 01:22:47 -0400

    Version bump for packaging script.

--- packaging_scripts/pack_dist.sh
+++ packaging_scripts/pack_dist.sh
@@ -2,7 +2,7 @@
 # A conviniance script to pack the release distrubutions into zip files.
 # This is a linux bash script using 7zip.
 # You can also just create the zip files maunually, nothing fancy going on here.
-VERSION=1.0.0
+VERSION=1.1.0
 mkdir ./../dist/packaged
 cd ./../dist/Eve\ Fleet\ Simulator-win32-x64
 7z a -tzip -mx=9 ./../packaged/Eve\ Fleet\ Simulator-$VERSION-win.zip ./*
